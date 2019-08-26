4abf0baf288ad47141f5e4d203ac1c2ebb7c51df
Rama Shah <ramashah@Ramas-MacBook.home>
2019-08-25 21:54:11 -0400

    adding due task and constructing que

--- due.js
+++ due.js
@@ -1,3 +1,4 @@
+//TODO: refactor
 let freeWorkersMap = new WeakMap();
 let capacityMap = new WeakMap();
 let concurrencyMap = new WeakMap();
@@ -11,11 +12,37 @@ let workingWorkersMap= new WeakMap();
 
 class Worker{
     constructor(work, capacity){
+        this.worker = work;
+        this.capacity = capacity;
+        this.working = false;
+        this.waiting = false;
+    }
+
+    async work(tasks){
+        await this.worker(tasks);
     }
 
 }
-class Cue{
+class Due{
     constructor(worker, capacity, concurrency, delay, empty, drain){
+        this.worker = worker;
+        capacityMap.set(this,capacity);
+        concurrencyMap.set(this,concurrency);
+        delayMap.set(this,delay);
+        emptyMap.set(this,empty);
+        drainMap.set(this,drain);
+        runningMap.set(this,false);
+        idleMap.set(this,true);
+        tasksMap.set(this,[]);
+        workingWorkersMap.set(this,0);
+
+        let workers = [];
+        for(let i = 0;i< concurrencyMap.get(this);i++){
+            workers.push(new Worker(this.worker,capacityMap.get(this)));
+        }
+
+        freeWorkersMap.set(this,workers);
+
     }
 
     get running(){
@@ -30,6 +57,19 @@ class Cue{
         return concurrencyMap.get(this);
     }
 
+    get length(){
+        return tasksMap.get(this);
+    }
+
+    set concurrency(concurrency){
+        concurrencyMap.set(this,concurrency);
+        let workers = [];
+        for(let i = 0;i< concurrencyMap.get(this);i++){
+            workers.push(new Worker(this.worker,capacityMap.get(this)));
+        }
+
+        freeWorkersMap.set(this,workers);
+    }
     async push(newTasks){
         return new Promise( async resolve =>{
             if((!!newTasks) && (newTasks.constructor === Array)){
@@ -40,17 +80,88 @@ class Cue{
             }else{
                 tasksMap.get(this).push(newTasks);
             }
+
+            if(tasksMap.get(this).length < capacityMap.get(this)){
+                this.waiting = true;
+                const sleep = m => new Promise(r => { this.waitingLoop = setTimeout(r, m)});
+
+                await sleep(delayMap.get(this));
+
+                if(this.waiting = true){
+                    this.waiting = false;
+                    await this.startWorking();
+                }
+            }else{
+                if(this.waiting === true){
+                    this.waiting = false;
+                    clearTimeout(this.waitingLoop);
+                }
+                await this.startWorking();
+            }
+
+            drainMap.get(this)();
             resolve()
         });
     }
 
+    async startWorking(){
+        new Promise(async resolve =>{
+            runningMap.set(this,true);
+            idleMap.set(this,false);
+            await this.offerWork();
+            resolve()
+        })
+    }
+
+    async offerWork(){
+        new Promise(async resolve => {
+            if(tasksMap.get(this).length === 0){
+                runningMap.set(this,false);
+                idleMap.set(this,true)
+            }else{
+                //TODO: refactor
+                while(freeWorkersMap.get(this).length > 0 && tasksMap.get(this).length > 0){
+                    if(workingWorkersMap.get(this) !== concurrencyMap.get(this)){
+                        workingWorkersMap.set(this,workingWorkersMap.get(this)+1);
+                        let freeWorker =  freeWorkersMap.get(this).pop();
+                        let doneWorker = await this.workerStart(freeWorker);
+                        workingWorkersMap.set(this,workingWorkersMap.get(this)-1);
+                        if(freeWorkersMap.get(this) + 1 <= concurrencyMap.get(this)) freeWorkersMap.get(this).unshift(doneWorker);
+                        await this.offerWork()
+                    }
+                }
+            }
+            resolve()
+        })
+    }
+
+    async workerStart(worker){
+        new Promise(async resolve =>{
+            let toDoTasks = tasksMap.get(this).splice(0,capacityMap.get(this));
+            if(tasksMap.get(this).length === 0) emptyMap.get(this)();
+            await worker.work(toDoTasks);
+            return worker
+        });
+    }
 }
 
-function cue(worker,capacity,concurrency,delay = 0){
-
-//TODO
+function due(worker,capacity,concurrency = 1, delay = 0){
+    //TODO: refactor
+    if((typeof(worker) === "object" || typeof(capacity) === "object") && worker !== undefined && capacity !== undefined){
+        let workerNumber = typeof(worker) === "object" ? capacity : worker;
+        let options = typeof(capacity) === "object" ? capacity : worker;
+        capacity = options.capacity || 1;
+        concurrency = options.concurrency || 1;
+        delay = options.delay || 1;
+        let emptyFunc = () => {};
+        let empty = options.empty || emptyFunc;
+        let drain = options.drain || emptyFunc;
+        return new Due(workerNumber,capacity,concurrency,delay,empty,drain)
+    }else if(worker !== undefined && capacity !== undefined){
+        return new Due(worker,capacity,concurrency,delay,()=>{},()=>{})
+    }
+    throw Error("Input Mismatch");
 }
 
 
-
-module.exports = cue;
+module.exports = due;
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-//TODO
-
 const due = require('./due');
+
+function worker (tasks, name) {
+    for(let i = 0;i < tasks.length;i++){
+        console.log(`Doing task ${tasks[i]}`);
+    }
+}
+
+//TODO: refactor
+// Create a due of 5 workers, // each one processing up to 20 tasks
+const workers = due( { empty: () => { console.log("Empty !") }, drain : ()=>{
+        console.log("drain !")} , delay : 3000, capacity : 3 , concurrency : 2 }, worker);
+
+// Add a task to be processed by the workers
+task = [1, 2, 3, 4, 5, 6,];
+
+workers.push(task).then( op =>{
+    console.log("Done");
+});
+
+
